822cf01fa96eac1bf8e26631c9db64daefc35eca
TormodSanti <tormodsanti@gmail.com>
2021-11-25 21:45:35 +0100

    Update funscript.js
    Spaghetti code

--- funscript.js
+++ funscript.js
@@ -21,24 +21,36 @@ let runding = document.getElementById("firkant");
 runding.style.visibility = "hidden";
 
 
-
+/*  Because this code measures how far you have scrolled, and the timeline will be narrower or wider depending on your screen size, 
+    it will ONLY work with a screen width of 1920px. We were about to scrap this idea, but decided to let it stay, and hide the element for other resolutions.*/
 
 $(timeline).on('scroll', function () {
     console.log("HORISONTAL = " + $(this).scrollTop());
     var number = $(this).scrollTop();
      if (number >= 10 && number < 4400) {
-         runding.innerHTML = "1: Global Population: <br/>" +   (Math.floor(number) * 3);
+         runding.innerHTML = "Global Population (approximate): <br/>" +   (Math.floor(number) * 4 + 10000);
          // breakpoint 100.000 BCE => number = 4300
      } else if (number > 4400 && number < 8000) {
-         runding.innerHTML = "2: Global Population: <br/>" + ((Math.floor(number) * 6));
+         runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-4400) * 16 + Math.floor(4400) * 4  + 10000);
          // breakpoint 9000 bce = 9300
      } else if (number > 8000 && number < 9300) {
-         runding.innerHTML = "3: Global Population: <br/>" + ((Math.floor(number) * 12));
+         runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-8000) * 25 + Math.floor(8000-4400) * 16 + Math.floor(4400) * 4  + 10000);
      }
-     else if (number > 9300) {
-         runding.innerHTML = "4: Global Population: <br/>" + ((Math.floor(number) * 100));
+     else if (number > 9300 && number < 17500) {
+         runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-9300) * 260 + Math.floor(9300-8000) * 25 + Math.floor(8000-4400) * 16 + Math.floor(4400) * 4  + 10000);
     }
-
+    else if (number > 17500 && number < 30000) {
+        runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-17500) * 21000 + Math.floor(17500-9300) * 260 + Math.floor(9300-8000) * 25 + Math.floor(8000-4400) * 16 + Math.floor(4400) * 4  + 10000);
+    }
+    else if (number > 30000 && number < 34000) {
+        runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-30000) * 40000 + Math.floor(30000-17500) * 21000 + Math.floor(17500-9300) * 260 + Math.floor(9300-8000) * 25 + Math.floor(8000-4400) * 16 + Math.floor(4400) * 4  + 10000);
+    }
+    else if (number > 34000 && number < 45000) {
+        runding.innerHTML = "Global Population: (approximate)<br/>" + (Math.floor(number-34000) * 320000 + Math.floor(34000-30000) * 40000 + Math.floor(30000-17500) * 21000 + Math.floor(17500-9300) * 260 + Math.floor(9300-8000) * 25 + Math.floor(8000-4400) * 16 + Math.floor(4400) * 4  + 10000);
+    }
+    else if (number > 45000) {
+        runding.innerHTML = "Global Population: (approximate)<br/>?????";
+    }
 })
 
 
